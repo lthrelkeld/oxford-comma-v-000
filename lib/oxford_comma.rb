626296471e1e30f61b453a5e67e1d6e7c93728f2
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 18:26:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
   array_with_comma = array.insert(-2,"and")
   last_element_of_array = array_with_comma.pop
   string = array_with_comma.join(", ")
+  string << " "
   string << last_element_of_array
   binding.pry
 end
