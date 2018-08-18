650ab6c508e631f34c89ce208717fd1136f943a2
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 18:32:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def oxford_comma(array)
 
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    array_with_comma = array.insert(-2,"and")
+    last_element_of_array = array_with_comma.pop
+    string = array_with_comma.join(", ")
+    string << " #{last_element_of_array}"
+  end
 end
