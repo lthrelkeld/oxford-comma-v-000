a81b4b42319127bd2317da14218ea3810d60bc70
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 18:29:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,16 @@
 require 'pry'
 def oxford_comma(array)
-  array_with_comma = array.insert(-2,"and")
-  last_element_of_array = array_with_comma.pop
-  string = array_with_comma.join(", ")
-  binding.pry
-  string << " "
-  string << last_element_of_array
-  
+
+  if array.length == 1
+    return array.to_s
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    array_with_comma = array.insert(-2,"and")
+    last_element_of_array = array_with_comma.pop
+    string = array_with_comma.join(", ")
+    binding.pry
+    string << " "
+    string << last_element_of_array
+  end
 end
