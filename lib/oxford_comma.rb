dbf4178a25922deb4683540b7b280619ce9b9df0
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 18:27:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
   array_with_comma = array.insert(-2,"and")
   last_element_of_array = array_with_comma.pop
   string = array_with_comma.join(", ")
+  binding.pry
   string << " "
   string << last_element_of_array
-  binding.pry
+  
 end
