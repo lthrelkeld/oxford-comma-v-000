5648a11cb65e6a02d9cc9eea7c918974635d3c5a
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 18:26:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
+require 'pry'
 def oxford_comma(array)
   array_with_comma = array.insert(-2,"and")
   last_element_of_array = array_with_comma.pop
   string = array_with_comma.join(", ")
   string << last_element_of_array
+  
 end
